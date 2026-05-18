0378c6557deb39833651bdf0576ff536ea2b33cd
Xniperwolf <ig.gutierrezh@duocuc.cl>
2026-05-18 18:53:24 -0400

    intentando ocupar docker compose moderno

--- main.tf
+++ main.tf
@@ -77,7 +77,8 @@ variable "user_data_docker" {
             curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
             add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
             apt-get update -y
-            apt-get install -y docker-ce docker-compose
+            # Instala docker y el plugin moderno de compose con espacio
+            apt-get install -y docker-ce docker-compose-plugin
             systemctl start docker
             systemctl enable docker
             usermod -aG docker ubuntu
